0f87d1d0f8a49ec352a999458709fe04f03ee7c9
Thiago Loure <thiagoloure.developer@gmail.com>
2025-02-07 07:56:48 -0300

    operadores aritimeticos

---
+++ tipos-variaveis/src/OperadoresAritimeticos.java
@@ -0,0 +1,26 @@
+public class OperadoresAritimeticos {
+    
+    public static void main(String[] args) {
+        String nomeCompleto = "Thiago" + " Loure";
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        System.out.println(concatenacao);
+        
+        concatenacao = 1 + 1 + "1";
+        System.out.println(concatenacao);
+        
+        concatenacao = 1 + "1" + 1 + "1";
+        System.out.println(concatenacao);
+        
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);
+        
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);
+        
+
+    }
+    
+}
