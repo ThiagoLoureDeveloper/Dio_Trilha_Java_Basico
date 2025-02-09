8d8812a58bdc4cb5a9e5dda6b2d5b09cd30e1634
Thiago Loure <thiagoloure.developer@gmail.com>
2025-02-08 21:42:47 -0300

    projeto banco

---
+++ tipos-variaveis/src/OperadoresTernarios.java
@@ -0,0 +1,17 @@
+public class OperadoresTernarios {
+
+    public static void main(String[] args) {
+        
+        int a = 6;
+        int b = 6;
+
+        String resultado = "";
+        if (a==b) 
+        resultado = "Verdadeiro";
+        else
+        resultado = "falso";
+
+        System.out.println(resultado);
+          
+    }
+}
